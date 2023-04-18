c2f68b90c747c9b0f29c248dfa1670e9e121ca58
Wiktor Bajor <wiktorbajor1@gmail.com>
2023-04-18 23:43:45 +0200

    Add reading and displaying map

---
+++ MazeSolver/AppWidgets/Colors.py
@@ -0,0 +1,13 @@
+import colorspy as color
+
+
+RED = color.red
+GREEN = color.green
+BLUE = color.blue
+YELLOW = color.yellow
+WHITE = color.white
+BLACK = color.black
+PURPLE = color.purple
+ORANGE = color.orange
+GREY = color.gray
+TURQUOISE = color.turquoise
--- MazeSolver/MazeSolver.py
+++ MazeSolver/MazeSolver.py
@@ -1,6 +1,6 @@
 import pygame
 import numpy as np
-
+from AppWidgets.Colors import GREEN
 
 def getPointsFromFile():
     points = np.genfromtxt('maze.txt', delimiter=',', dtype=np.int32)
@@ -9,15 +9,29 @@ def getPointsFromFile():
     end = points[1]
     return points[2:], start, end
 
+def createObstacleList(points):
+    obstacleSize = 30
+    obstacleList = []
+    for x, y in points:
+        rect = pygame.Rect((x,y),(obstacleSize, obstacleSize))
+        obstacleList.append(rect)
+
+    return obstacleList
+
+def drawObstacles(obstacleList, window):
+    for rect in obstacleList:
+        pygame.draw.rect(window,GREEN,rect)
+
 def main():
     pygame.init()
-    points, start, end = getPointsFromFile()
+    points, _, _ = getPointsFromFile()
+    windowSize = (900,600)
+    screen = pygame.display.set_mode(windowSize)
+    obstacleList = createObstacleList(points)
 
-    screen = pygame.display.set_mode((900, 720))
 
     while True:
-
-
+        drawObstacles(obstacleList, screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
