96f90afe5d9ca00c6bc570e9d8219ab2005f3c95
Wiktor Bajor <wiktorbajor1@gmail.com>
2023-05-17 20:26:39 +0200

    Add simulation

--- MazeSolver/MazeSolver.py
+++ MazeSolver/MazeSolver.py
@@ -4,6 +4,7 @@ from AppWidgets.Colors import GREEN, BLACK, RED
 from AppWidgets.Button import Button
 from PathCreateAlgorithms.Voronoi import VornoiPathFinder
 import PathCreateAlgorithms.rrt as RRT
+from Robot.Robot import Robot
 
 def getPointsFromFile():
     points = np.genfromtxt('MazeSolver/assets/maze.txt', delimiter=',', dtype=np.int32)
@@ -43,7 +44,7 @@ def createButtons():
 
     return { "voronoi" : voronoi_button, "rrt" : rrt_button}
 
-def simulationLoop():
+def simulationLoop(robot, path, screen, obstacleList):
     run = True
     while run:
         pygame.display.update()
@@ -53,8 +54,15 @@ def simulationLoop():
                 exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == ord ( "m" ):
+                    pygame.display.set_caption("Menu")
                     run = False
 
+        screen.fill(BLACK)
+        drawPathPoints(path, screen)
+        drawObstacles(obstacleList, screen)
+        robot.moveRobot(path)
+        robot.draw(screen)
+        pygame.display.update()
 
 def main():
     pygame.init()
@@ -62,6 +70,7 @@ def main():
     windowSize = (900,600)
     screen = pygame.display.set_mode(windowSize)
     obstacleList = createObstacleList(points)
+    robotRadius = round(10 * 2**(0.5))
     pygame.display.set_caption("Menu")
 
     back_ground = pygame.image.load("MazeSolver/assets/Background.png")
@@ -85,21 +94,23 @@ def main():
                 exit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-
+                robot = Robot((start[0], start[1]), robotRadius)
                 if buttons["voronoi"].checkForInput(menu_mouse_pos):
+                    pygame.display.set_caption("Voronoi")
                     screen.fill(BLACK)
+                    drawObstacles(obstacleList, screen)
+                    pygame.display.update()
                     voronoi = VornoiPathFinder(start, end, points)
                     path = voronoi.findVoronoiPath()
-                    drawPathPoints(path, screen)
-                    drawObstacles(obstacleList, screen)
-                    simulationLoop()
+                    simulationLoop(robot, path, screen, obstacleList)
 
                 if buttons["rrt"].checkForInput(menu_mouse_pos):
+                    pygame.display.set_caption("RRT")
                     screen.fill(BLACK)
                     drawObstacles(obstacleList, screen)
                     rrt = RRT.RRT(windowSize, start, end,obstacleList, points,screen)
                     path = RRT.findRRTPath(rrt)
-                    simulationLoop()
+                    simulationLoop(robot, path, screen, obstacleList)
 
             pygame.display.update()
 
